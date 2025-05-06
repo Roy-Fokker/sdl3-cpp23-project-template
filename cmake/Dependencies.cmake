3a88d1c1667b4c088b56baed3cc60a9d893b5e06
Roy-Fokker <11281115+Roy-Fokker@users.noreply.github.com>
2025-05-06 19:06:24 -0400

    update to SDL v3.2.12

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -2,5 +2,5 @@
 include(${CMAKE_MODULE_PATH}/CPM.cmake)
 
 # Dependencies
-CPMAddPackage("gh:libsdl-org/SDL#release-3.2.10")
+CPMAddPackage("gh:libsdl-org/SDL#release-3.2.12")
 CPMAddPackage("gh:g-truc/glm#release-1-0-2")
