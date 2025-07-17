00cf4fc5244ada89322d868fb51274d2549f9411
Roy-Fokker <11281115+Roy-Fokker@users.noreply.github.com>
2025-07-17 19:30:51 -0400

    update sdl3 version

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -2,5 +2,5 @@
 include(${CMAKE_MODULE_PATH}/CPM.cmake)
 
 # Dependencies
-CPMAddPackage("gh:libsdl-org/SDL#release-3.2.16")
+CPMAddPackage("gh:libsdl-org/SDL#release-3.2.18")
 CPMAddPackage("gh:g-truc/glm#release-1-0-2")
