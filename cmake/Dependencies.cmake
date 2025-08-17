71f1772455670493d376d37a5f3dbf1e214d97d4
Roy-Fokker <11281115+Roy-Fokker@users.noreply.github.com>
2025-08-17 09:54:03 -0400

    ensure GLM is configured to static lib

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -3,4 +3,11 @@ include(${CMAKE_MODULE_PATH}/CPM.cmake)
 
 # Dependencies
 CPMAddPackage("gh:libsdl-org/SDL#release-3.2.20")
-CPMAddPackage("gh:g-truc/glm#release-1-0-2")
+
+CPMAddPackage(
+	URI "gh:g-truc/glm#master"
+	OPTIONS                        # have to manually specify options
+	"GLM_BUILD_TESTS OFF"          # else it chooses to build Shared Library
+	"BUILD_STATIC_LIBS ON"         # This does not export .lib file with MSVC
+	"BUILD_SHARED_LIBS OFF"        # Seem GLM does not export any symbols (dllexport)
+)
